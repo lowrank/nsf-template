e2c7076eda2f38033c385aecd3058b678b769ca5
Yimin Zhong <yimin.zhong@duke.edu>
2022-11-30 16:52:40 -0600

    prepared all files

--- compile.sh
+++ compile.sh
@@ -10,6 +10,23 @@ bibtex                                   ./Submission/description.aux
 pdflatex --output-directory=./Submission ./Project_Description/description.tex
 pdflatex --output-directory=./Submission ./Project_Description/description.tex
 
+# Facilities Equipment and Other Resources
+pdflatex --output-directory=./Submission ./Facilities_Equipment_and_Other_Resources/resources.tex
+bibtex                                   ./Submission/resources.aux 
+pdflatex --output-directory=./Submission ./Facilities_Equipment_and_Other_Resources/resources.tex
+pdflatex --output-directory=./Submission ./Facilities_Equipment_and_Other_Resources/resources.tex
+
+# Data Management Plan
+pdflatex --output-directory=./Submission ./Data_Management_Plan/data.tex
+bibtex                                   ./Submission/data.aux 
+pdflatex --output-directory=./Submission ./Data_Management_Plan/data.tex
+pdflatex --output-directory=./Submission ./Data_Management_Plan/data.tex
+
+# Postdoc Mentoring Plan
+pdflatex --output-directory=./Submission ./Postdoctoral_Mentoring_Plan/postdoc.tex
+bibtex                                   ./Submission/postdoc.aux 
+pdflatex --output-directory=./Submission ./Postdoctoral_Mentoring_Plan/postdoc.tex
+pdflatex --output-directory=./Submission ./Postdoctoral_Mentoring_Plan/postdoc.tex
 
 # Clean up
 rm -rf ./Submission/*.aux ./Submission/*.bbl ./Submission/*.blg ./Submission/*.out ./Submission/*.log 
