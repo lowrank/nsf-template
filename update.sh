a7435ce3cdb34c980b6f6cfb532c1182625c8a16
YIMIN ZHONG <yzhong@utexas.edu>
2022-11-30 13:30:43 -0600

    add folders

---
+++ update.sh
@@ -0,0 +1,5 @@
+# update the repository
+git pull
+
+# download necessary forms
+curl -o Senior_Personnel_Documents/Biographical_Sketch.pdf https://www.nsf.gov/bfa/dias/policy/nsfapprovedformats/biosketch.pdf
