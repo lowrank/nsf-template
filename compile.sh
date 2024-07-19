628f08fd322c83dcab09108291bb844ff535f265
Yimin Zhong <yzz0225@auburn.edu>
2024-07-19 13:05:01 -0500

    update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 # Summary
 cd Project_Summary
 pdflatex --output-directory=../Submission summary.tex
-bibtex                                    ../Submission/summary.aux
+bibtex                      ../Submission/summary.aux
 pdflatex --output-directory=../Submission summary.tex
 pdflatex --output-directory=../Submission summary.tex
 cd ..
@@ -19,35 +19,35 @@ cd ..
 # References Cited
 cd References_Cited
 pdflatex --output-directory=../Submission references.tex
-bibtex                                    ../Subsmission/references.aux 
+bibtex                      ../Subsmission/references.aux 
 pdflatex --output-directory=../Submission references.tex
 pdflatex --output-directory=../Submission references.tex
 cd ..
 # Facilities Equipment and Other Resources
 cd Facilities_Equipment_and_Other_Resources
 pdflatex --output-directory=../Submission resources.tex
-bibtex                                    ../Submission/resources.aux 
+bibtex                      ../Submission/resources.aux 
 pdflatex --output-directory=../Submission resources.tex
 pdflatex --output-directory=../Submission resources.tex
 cd ..
 # Data Management Plan
 cd Data_Management_Plan
 pdflatex --output-directory=../Submission data.tex
-bibtex                                    ../Submission/data.aux 
+bibtex                      ../Submission/data.aux 
 pdflatex --output-directory=../Submission data.tex
 pdflatex --output-directory=../Submission data.tex
 cd ..
 # Postdoc Mentoring Plan
 cd Mentoring_Plan
 pdflatex --output-directory=../Submission mentoring.tex
-bibtex                                    ../Submission/mentoring.aux 
+bibtex                      ../Submission/mentoring.aux 
 pdflatex --output-directory=../Submission mentoring.tex
 pdflatex --output-directory=../Submission mentoring.tex
 cd ..
 # Postdoc Mentoring Plan
 cd Senior_Personnel_Documents
 pdflatex --output-directory=../Submission synergistic.tex
-bibtex                                    ../Submission/synergistic.aux 
+bibtex                      ../Submission/synergistic.aux 
 pdflatex --output-directory=../Submission synergistic.tex
 pdflatex --output-directory=../Submission synergistic.tex
 cd ..
